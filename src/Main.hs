4893f8bc1dd15cd6bc19af65d8d5c66ed26b69f4
Diogo Miyake <akio.miyake@live.com>
2022-08-22 17:37:23 -0300

    update

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,23 @@
-import SortingAlgorithms
+{-# LANGUAGE OverloadedLabels  #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import           Data.GI.Base
+import qualified GI.Gtk             as Gtk
+import           SortingAlgorithms 
 
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  Gtk.init Nothing
+
+  win <- new Gtk.Window [#title := "Functional Sorting Algoritms"]
+  on win #destroy Gtk.mainQuit
+  #resize win 640 480
+
+  msg <- new Gtk.Label [#label := "Hello"]
+  #add win msg
+
+  #showAll win
+
+  Gtk.main
